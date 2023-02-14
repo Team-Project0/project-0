94198953b0072d37483acb6b9c417ed60ec3e82b
eyazeddini <eya.zeddini22@gmail.com>
2023-02-14 17:43:50 +0100

    ee

--- server/database-mysql/index.js
+++ server/database-mysql/index.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'sadmedous',
-  password : 'iLUVmFrlF<3',
-  database : 'mvp'
-});
+  user     : 'root',
+  password : 'user',
+  database : 'mydb'
+}).promise()
+connection.connect().then(()=>console.log("db connected")).catch(err=>console.log(err))
 
 module.exports = connection;
